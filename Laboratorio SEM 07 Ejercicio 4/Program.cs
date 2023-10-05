90fe6e09aeebd04169c67f2ba923022fd3d47809
Oscar Jair Cuadros Nuñez <N00377634@upn.pe>
2023-10-04 23:55:29 -0500

    Add project files.

---
+++ Laboratorio SEM 07 Ejercicio 4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ingrese un número: ");
+
+int altura = int.Parse(Console.ReadLine());
+
+for (int numEscaleras = 1; numEscaleras <= altura; numEscaleras++)
+{
+    for (int i = 1; i < numEscaleras; i++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine("*");
+}
